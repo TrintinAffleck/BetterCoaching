2c8b98c2ccbc2d852eee0291a1c153e7343b2908
Trintin Affleck <t.affleck101@hotmail.com>
2022-12-09 14:23:21 -0700

    modified profile_img

--- users/models.py
+++ users/models.py
@@ -8,13 +8,11 @@ class Profile(models.Model):
     name = models.CharField(max_length = 100, blank = True, null = True)
     email = models.CharField(max_length = 100, blank = True, null = True)
     is_coach = models.BooleanField()
-    if is_coach == True:
-        profile_img = models.ImageField(null = True, blank = True, upload_to = 'profiles/', default = 'profiles/user-default.png')
-        summoner_link = models.CharField(max_length = 250, blank = True, null = True)
+    profile_img = models.ImageField(null = True, blank = True, upload_to = 'profiles/', default = 'profiles/user-default.png')
     discord_link = models.CharField(max_length = 50, null = True, blank = True)
     created_date = models.DateTimeField(auto_now_add = True)
     id = models.UUIDField(default = uuid.uuid4, unique = True, 
                           primary_key = True, editable = False)
 
     def __str__(self):
-        return str(self.name)
+        return str(self.username)
