ebab8300717ddde9ccf69b6a8fe81da5c8006b18
Trintin Affleck <t.affleck101@hotmail.com>
2023-01-09 14:56:52 -0700

    Unused Imports

--- users/forms.py
+++ users/forms.py
@@ -1,8 +1,8 @@
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
-from django.forms import ModelForm, Select
+from django.forms import ModelForm
 from .models import Profile
-from coaches.models import Coach
+
 
 class CustomUserCreationForm(UserCreationForm):
     class Meta:
