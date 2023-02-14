a50a519d6f64af33f9b18b417ef09e767f6e07d6
Trintin Affleck <t.affleck101@hotmail.com>
2023-02-13 18:03:28 -0700

    rm key

--- BetterCoaching/settings.py
+++ BetterCoaching/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-6)oejbri2y^g3rs4d*5j9sihm7+(m*#!bfq$n8h+vv7saw%mtd'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
