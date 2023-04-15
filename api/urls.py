6061ebf989f01130535ddd4dd7833f3326d2abb7
Trintin Affleck <t.affleck101@hotmail.com>
2023-04-15 11:53:54 -0600

    API routes and serializers

--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
 from . import views
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
-    path('', views.getRoutes, name='api'),
-
+    path('', views.getRoutes),
+    path('coaches', views.getCoaches),
+    path('coach/<str:pk>', views.getCoach),
+    path('coach/<str:pk>/vote', views.coachReview),
+    path('users/token', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('users/token/refresh', TokenRefreshView.as_view(), name='token_refresh'),
 ]
--- api/views.py
+++ api/views.py
@@ -1,15 +1,49 @@
-from django.http import JsonResponse
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from coaches.models import Coach, Review
+from .serializers import CoachSerializer
 
+@api_view(['GET'])
 def getRoutes(request):
     routes = [
-        {'GET':'/api/coaches'},
-        {'GET':'/api/coaches/id'},
-        {'POST':'/api/coaches/id/review'},
+        {'GET': '/api/coaches'},
+        {'GET': '/api/coaches/id'},
+        {'POST': '/api/coaches/id/review'},
 
-        {'POST':'/api/users/token'},
-        {'POST':'/api/users/token/refresh'},
+        {'POST': '/api/users/token'},
+        {'POST': '/api/users/token/refresh'},
 
     ]
 
-    return JsonResponse(routes, safe=False, json_dumps_params={'indent' : 2, 'sort_keys' : True})
+    return Response(routes)
 
+@api_view(['GET'])
+def getCoaches(request):
+    coaches = Coach.objects.all()
+    serializer = CoachSerializer(coaches, many=True)
+    return Response(serializer.data)
+
+@api_view(['GET'])
+def getCoach(request, pk):
+    coach = Coach.objects.get(display_name=pk)
+    serializer = CoachSerializer(coach, many=False)
+    return Response(serializer.data)
+
+
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
+def coachReview(request, pk):
+    coach = Coach.objects.get(id=pk)
+    review = Review.objects.create(
+        owner = request.user.profile,
+        coach = coach
+    )
+
+    review.rating_value = request.data['rating_value']
+    review.body = request.data['body']
+    review.save()
+    coach.get_votes
+
+    serializer = CoachSerializer(coach, many=False)
+    return Response(serializer.data)
