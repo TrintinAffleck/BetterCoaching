abf373e1aaa020825fd924db6f280613529ce0b1
Trintin Affleck <t.affleck101@hotmail.com>
2023-04-26 11:36:10 -0600

    Unused Imports

--- users/views.py
+++ users/views.py
@@ -5,8 +5,6 @@ from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
 from coaches.models import Coach
 from .forms import CustomUserCreationForm, UpdateAccountForm, MessageForm
-from django.shortcuts import get_object_or_404
-from django.http.response import Http404
 
 def loginUser(request):
     page = 'login'
